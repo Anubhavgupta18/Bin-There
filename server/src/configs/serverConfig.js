ad1e156dafa360cd5f336006ae9a358b4f230d4a
anubhav <anubhavshivahre@gmail.com>
2023-03-17 21:27:16 +0530

    added image upload functionality

--- server/src/configs/serverConfig.js
+++ server/src/configs/serverConfig.js
@@ -7,5 +7,9 @@ module.exports = {
     MONGODB_URL: process.env.MONGODB_URL,
     JWT_SECRETKEY: process.env.JWT_SECRETKEY,
     EMAIL_ID:process.env.EMAIL_ID,
-    EMAIL_PASS:process.env.EMAIL_PASS
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    BUCKET_NAME: process.env.BUCKET_NAME
 }

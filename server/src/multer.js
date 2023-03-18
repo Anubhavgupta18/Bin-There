84eca3339781810e8ed1e8291f368cc2d75cb3e8
Sudhanshu <sudhanshu.mittal.49@gmail.com>
2023-03-18 22:53:21 +0530

    removed file filter

--- server/src/multer.js
+++ server/src/multer.js
@@ -2,13 +2,13 @@ const multer = require('multer');
 
 module.exports = multer({
     storage: multer.diskStorage({}),
-    fileFilter: (req, file, cb) => {
-        if (!file.mimetype.match('image/jpeg|image/png|image/gif|image/webp')) {  //image/jpeg contains both jpeg and jpg
-            console.log('wrong');
-            console.log(file.mimetype);
-            cb(new Error('File is not supported'), false)
-            return
-        }
-        cb(null, true)
-    }
+    // fileFilter: (req, file, cb) => {
+    //     if (!file.mimetype.match('image/jpeg|image/png|image/gif|image/webp')) {  //image/jpeg contains both jpeg and jpg
+    //         console.log('wrong');
+    //         console.log(file.mimetype);
+    //         cb(new Error('File is not supported'), false)
+    //         return
+    //     }
+    //     cb(null, true)
+    // }
 })

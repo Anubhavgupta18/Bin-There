822c1ddbfee3d470386581b313bc0d585b052b2f
Sudhanshu <sudhanshu.mittal.49@gmail.com>
2023-03-19 10:40:28 +0530

    changed pickup

--- server/src/routes/pickup-routes.js
+++ server/src/routes/pickup-routes.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', authenticateUser, createPickup);
-router.patch('/update/:id', authenticateUser, updatePickup);
+// router.patch('/update/:id', authenticateUser, updatePickup);
 router.get('/user/:id', findPickUpsByUserId )
 router.patch('/update/:pickUpId', authenticateUser, updatePickup);
 router.get('/user', authenticateUser, findPickUpsByUserId )
